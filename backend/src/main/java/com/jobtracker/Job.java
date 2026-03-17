1bceedccffdeeda846aa8aceb60756578d68dd0a
Srija <talamarlasrija@gmail.com>
2026-03-18 01:01:57 +0530

    Add Job class

---
+++ backend/src/main/java/com/jobtracker/Job.java
@@ -0,0 +1,46 @@
+package com.jobtracker;
+
+public class Job {
+    private Long id;
+    private String companyName;
+    private String jobTitle;
+    private String status;
+
+    public Job(String companyName, String jobTitle, String status) {
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
